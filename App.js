25b03a1e4896fd8d334eb5856122c11e4b30c9d2
Ankit Soni <56527089+ankitsoni4448@users.noreply.github.com>
2023-04-04 15:23:46 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,34 @@
+const btnEl = document.getElementById("btn");
+
+function createNoteEl(id, content){
+    //console.log(id, content);
+    const element = document.createElement("textarea")
+    element.classList.add("note")
+    element.placeholder = "Empty NOte"
+    element.value = content
+
+    element.addEventListener("dblclick", ()=>{
+        const warning = confirm("Do You Want To Delete This Note?")
+        if(warning){
+            deleteNote(id, element)
+        }
+    })
+}
+
+function deleteNote(){
+    
+}
+
+function addNote(){
+     const noteObj = {
+        id: Math.floor(Math.random() * 100000),
+        content: ""
+     };
+     //console.log(noteObj);
+    //console.log("clicked");
+    const noteEl = createNoteEl (noteObj.id, noteObj.content)
+
+
+}
+
+btnEl.addEventListener("click", addNote)
